cd44d4f1f732a96cdd38465b7446bc09da571071
fatma abozeina <fatmaabozeina142@gmail.com>
2022-02-21 13:09:02 +0200

    remove unused tween animation drawer. vreate FODMAPs and IBS page.

---
+++ lib/features/home/presentation/pages/FODMAPs_IBS.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class FODMAPsAndIBS extends StatelessWidget {
+  const FODMAPsAndIBS({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
