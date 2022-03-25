a0a9bba7690fa377afbd07f6e2835b3eefc51ceb
fatma abozeina <fatmaabozeina142@gmail.com>
2022-03-26 00:18:50 +0200

    solve expansion tile radio ontap problem,change expansion panel radio into expansion panel , remove silder backage to use slider widget instead,, creating database to add some values. create default form field widget to reuse.

--- lib/features/home/presentation/pages/home_drawer/diary/add_meal_dialog.dart
+++ lib/features/home/presentation/pages/home_drawer/diary/add_meal_dialog.dart
@@ -14,77 +14,262 @@ class _AddMealState extends State<AddMeal> {
   TextEditingController typeController = TextEditingController();
   TextEditingController dateController = TextEditingController();
   TextEditingController timeController = TextEditingController();
+  double sliderValue = 20;
+
+  List<bool> isOpen = [false, false, false];
+
+  // @override
+  // void setState(VoidCallback fn) {
+  //   super.setState(fn);
+  // }
+
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
       title: Text('add your notes'),
       content: SingleChildScrollView(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
+        // child: Column(
+        //   mainAxisSize: MainAxisSize.min,
+        //   children: [
+        // ExpansionTile(
+        //   expandedCrossAxisAlignment: CrossAxisAlignment.start,
+        //   title: Text(
+        //     'Meal',
+        //   ),
+        //   // subtitle: Text('choose type to add'),
+        //   children: [
+        //     DefaultFormField(
+        //         prefix: Icons.title,
+        //         keyboard: TextInputType.name,
+        //         hint: 'Type',
+        //         fieldController: typeController),
+        //     SizedBox(
+        //       height: 10,
+        //     ),
+        //     DefaultFormField(
+        //         prefix: Icons.calendar_today,
+        //         keyboard: TextInputType.datetime,
+        //         hint: 'Date',
+        //         ontap: () {
+        //           showDatePicker(
+        //                   context: context,
+        //                   initialDate: DateTime.now(),
+        //                   firstDate: DateTime.now(),
+        //                   lastDate: DateTime.parse('2022-05-09'))
+        //               .then((value) {
+        //             dateController.text = DateFormat.yMMMd().format(value!);
+        //           });
+        //         },
+        //         fieldController: dateController),
+        //     SizedBox(
+        //       height: 10,
+        //     ),
+        //     DefaultFormField(
+        //         prefix: Icons.watch_later_outlined,
+        //         keyboard: TextInputType.datetime,
+        //         ontap: () {
+        //           print('time taped');
+        //           showTimePicker(
+        //                   context: context,
+        //                   initialTime: TimeOfDay.now(),
+        //                   initialEntryMode: TimePickerEntryMode.input)
+        //               .then((value) {
+        //             if (value != null) {
+        //               timeController.text =
+        //                   value.format(context).toString();
+        //             }
+        //           });
+        //         },
+        //         hint: 'Time',
+        //         fieldController: timeController),
+        //     Row(
+        //       mainAxisAlignment: MainAxisAlignment.end,
+        //       children: [
+        //         TextButton(
+        //             onPressed: () {
+        //               Navigator.pop(context);
+
+        //               print('canceled');
+        //             },
+        //             child: Text('Cancel')),
+        //         TextButton(
+        //             onPressed: () {
+        //               // if (widget.mealAdded != null) {
+        //               //   widget.mealAdded!(typeController.text);
+        //               // }
+
+        //               Navigator.pop(context);
+        //             },
+        //             child: Text('Save'))
+        //       ],
+        //     ),
+        //   ],
+        //   onExpansionChanged: (expand) {
+        //     setState(() {
+        //       expand ? isExpand2 == false : isExpand2 == true;
+        //     });
+        //   },
+        // ),
+        child: ExpansionPanelList(
+          expansionCallback: (i, isopen) {
+            setState(() {
+              isOpen?[i] = !isopen;
+            });
+          },
           children: [
-            ExpansionTile(
-              expandedCrossAxisAlignment: CrossAxisAlignment.start,
-              title: Text(
-                'Meal',
-              ),
-              // subtitle: Text('choose type to add'),
-              children: [
-                DefaultFormField(
-                    prefix: Icons.title,
-                    keyboard: TextInputType.name,
-                    hint: 'Type',
-                    fieldController: typeController),
-                SizedBox(
-                  height: 10,
-                ),
-                DefaultFormField(
-                    prefix: Icons.date_range,
-                    keyboard: TextInputType.datetime,
-                    hint: 'Date',
-                    fieldController: dateController),
-                SizedBox(
-                  height: 10,
-                ),
-                DefaultFormField(
-                    prefix: Icons.title,
-                    keyboard: TextInputType.text,
-                    hint: 'Time',
-                    fieldController: timeController),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
+            ExpansionPanel(
+                isExpanded: isOpen![0],
+                // canTapOnHeader: true,
+                headerBuilder: (context, isexpanded) {
+                  return ListTile(
+                      title: Text(
+                    'Meal',
+                  ));
+                },
+                body: Column(
                   children: [
-                    TextButton(
-                        onPressed: () {
-                          Navigator.pop(context);
-
-                          print('canceled');
-                        },
-                        child: Text('Cancel')),
-                    TextButton(
-                        onPressed: () {
-                          // if (widget.mealAdded != null) {
-                          //   widget.mealAdded!(typeController.text);
-                          // }
-
-                          insertToDatabase().then((value) {
-                            print('wow');
+                    DefaultFormField(
+                        prefix: Icons.title,
+                        keyboard: TextInputType.name,
+                        hint: 'Type',
+                        fieldController: typeController),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    DefaultFormField(
+                        prefix: Icons.calendar_today,
+                        keyboard: TextInputType.datetime,
+                        hint: 'Date',
+                        ontap: () {
+                          showDatePicker(
+                                  context: context,
+                                  initialDate: DateTime.now(),
+                                  firstDate: DateTime.now(),
+                                  lastDate: DateTime.parse('2022-05-09'))
+                              .then((value) {
+                            dateController.text =
+                                DateFormat.yMMMd().format(value!);
                           });
-                          Navigator.pop(context);
                         },
-                        child: Text('Save'))
+                        fieldController: dateController),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    DefaultFormField(
+                        prefix: Icons.watch_later_outlined,
+                        keyboard: TextInputType.datetime,
+                        ontap: () {
+                          print('time taped');
+                          showTimePicker(
+                                  context: context,
+                                  initialTime: TimeOfDay.now(),
+                                  initialEntryMode: TimePickerEntryMode.input)
+                              .then((value) {
+                            if (value != null) {
+                              timeController.text =
+                                  value.format(context).toString();
+                            }
+                          });
+                        },
+                        hint: 'Time',
+                        fieldController: timeController),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        TextButton(
+                            onPressed: () {
+                              Navigator.pop(context);
+
+                              print('canceled');
+                            },
+                            child: Text('Cancel')),
+                        TextButton(
+                            onPressed: () {
+                              // if (widget.mealAdded != null) {
+                              //   widget.mealAdded!(typeController.text);
+                              // }
+
+                              Navigator.pop(context);
+                            },
+                            child: Text('Save'))
+                      ],
+                    ),
                   ],
-                )
-              ],
-            ),
-            ExpansionTile(
-              title: Text('Stress'),
-            ),
-            ExpansionTile(
-              title: Text('Symptoms'),
+                )),
+            ExpansionPanel(
+              isExpanded: isOpen![1],
+              headerBuilder: (context, isexpanded) {
+                return ListTile(
+                  title: Row(
+                    children: [Text('Stress : '), getStressLevel(sliderValue)],
+                  ),
+                );
+              },
+              body: Slider.adaptive(
+                value: sliderValue,
+                onChanged: (newValue) {
+                  setState(() {
+                    sliderValue = newValue;
+                  });
+                },
+                min: 0,
+                max: 100,
+                divisions: 10,
+                label: "$sliderValue%",
+              ),
             )
           ],
         ),
+
+        // ExpansionTile(
+        //   title: Row(
+        //     children: [Text('Stress : '), getStressLevel(sliderValue)],
+        //   ),
+        //   onExpansionChanged: (expand) {
+        //     setState(() {
+
+        //     });
+
+        //   },
+
+        //   children: [
+        //     Slider.adaptive(
+        //       value: sliderValue,
+        //       onChanged: (newValue) {
+        //         setState(() {
+        //           sliderValue = newValue;
+        //         });
+        //       },
+        //       min: 0,
+        //       max: 100,
+        //       divisions: 10,
+        //       label: "$sliderValue%",
+        //     ),
+        //   ],
+        // ),
+        // ExpansionTile(
+        //   title: Text('Symptoms'),
+        // )
+        // ],
+        // ),
       ),
     );
+  }
+}
+
+Widget getStressLevel(double value) {
+  if (value >= 0 && value < 20) {
+    return Text(
+      'low',
+      style: TextStyle(color: Colors.green),
+    );
+  } else if (value >= 20 && value < 40) {
+    return Text('medium', style: TextStyle(color: Colors.orange));
+  } else if (value >= 40 && value < 60) {
+    return Text('high', style: TextStyle(color: Colors.redAccent));
+  } else if (value >= 60 && value < 80) {
+    return Text('vary high', style: TextStyle(color: Colors.red));
+  } else {
+    return Text('Extreme', style: TextStyle(color: Colors.red));
   }
 }
